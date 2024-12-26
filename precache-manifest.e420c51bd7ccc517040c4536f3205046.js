a3a28a1b2725663d5cd83732dc8620e10d603035
Dasha <158553572+tarassiky@users.noreply.github.com>
2024-12-26 14:00:48 +0300

    Add files via upload

---
+++ precache-manifest.e420c51bd7ccc517040c4536f3205046.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d2410cbccde21a8dcb65a90b97642d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0715d923133aac0e1f0",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "fa2d8ee02cf184fc4314",
+    "url": "/static/js/2.3ad75f56.chunk.js"
+  },
+  {
+    "revision": "d0715d923133aac0e1f0",
+    "url": "/static/js/main.0912b35d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
